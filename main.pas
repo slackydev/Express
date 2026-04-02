546bdb701ec0cba40a1dedd29d275d9656c29198
Olly <Olly.cain@outlook.com>
2026-04-02 03:33:39 +0100

    Track memory allocation call count (GetMem & AllocMem). Might be of use one day.

--- main.pas
+++ main.pas
@@ -19,6 +19,31 @@ uses
   ,Windows  // UTF-8
   {$ENDIF};
 
+const
+  TrackMemoryAllocCount = True;
+
+var
+  MemTracking: record
+    Enabled: Boolean;
+    Count: Int64;
+    MemManager: TMemoryManager;
+    DefaultMemManager: TMemoryManager;
+  end;
+
+function MyAllocMem(Size:ptruint):Pointer;
+begin
+  if MemTracking.Enabled then
+    Inc(MemTracking.Count);
+  Result := MemTracking.DefaultMemManager.AllocMem(Size);
+end;
+
+function MyGetMem(Size:ptruint):Pointer;
+begin
+  if MemTracking.Enabled then
+    Inc(MemTracking.Count);
+  Result := MemTracking.DefaultMemManager.GetMem(Size);
+end;
+
 procedure RunScript(const AFileName: string);
 var
   Script: TExpress;
@@ -35,7 +60,10 @@ begin
   // --- COMPILATION ---
   WriteFancy('Compiling...');
 
+  MemTracking.Enabled := True;
+  MemTracking.Count := 0;
   Script.CompileFile(AFileName);
+  MemTracking.Enabled := False;
 
   WriteFancy(Script.BC.ToString(True));
 
@@ -45,17 +73,27 @@ begin
   WriteFancy('Emitted Bytecode in %.3f ms', [Script.BytecodeEmitTimeMs]);
   WriteFancy('Total compile time: %.3f ms', [Script.TotalCompileTimeMs]);
   WriteFancy('Memory used for compilation: %.4f MB', [Script.CompileMemoryUsedMb]);
+  if TrackMemoryAllocCount then
+    WriteFancy('Memory allocation calls for complilation: %d', [MemTracking.Count]);
+
   WriteLn;
 
   // --- EXECUTION ---
   WriteLn('Executing...');
+
+  MemTracking.Count := 0;
+  MemTracking.Enabled := True;
+
   exec_t := MarkTime();
-
   Script.Run();
-
   exec_t := MarkTime() - exec_t;
+
+  MemTracking.Enabled := True;
+
   WriteFancy('Executed in %.3f ms', [exec_t]);
   WriteFancy('Memory spilled in execution: %d bytes', [Script.MemorySpilled]);
+  if TrackMemoryAllocCount then
+    WriteFancy('Memory allocation calls in execution: %d', [MemTracking.Count]);
 
   // --- Example of getting a result back ---
   //resultVar := Script.GetVar('a');
@@ -81,6 +119,17 @@ end;
 var
   fileName:string;
 begin
+  if TrackMemoryAllocCount then
+  begin
+    MemTracking.Count := 0;
+    MemTracking.Enabled := False;
+    GetMemoryManager(MemTracking.DefaultMemManager);
+    MemTracking.MemManager := MemTracking.DefaultMemManager;
+    MemTracking.MemManager.AllocMem := @MyAllocMem;
+    MemTracking.MemManager.GetMem := @MyGetMem;
+    SetMemoryManager(MemTracking.MemManager);
+  end;
+
   {$IFDEF WINDOWS}
   SetConsoleOutputCP(CP_WINUNICODE);  // UTF-8
   {$ENDIF}
